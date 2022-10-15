02b0a9e3da992a9ec5cd740fee72d32bd0d69ff6
kshitijgundale <kshitijgundale08@gmail.com>
2022-10-15 20:33:07 +0530

    added TfidfVectorizer component

---
+++ optuna_automl/components/data_preprocessing/encoding/tfidf_vectorizer.py
@@ -0,0 +1,30 @@
+from sklearn.feature_extraction.text import TfidfVectorizer as TV
+from optuna_automl.registry import Registry, TEXT_ENCODING
+from sklearn.base import TransformerMixin
+from optuna_automl.hyperparameters import CategoricalHyperparameter, IntegerHyperparameter
+import json
+import pandas as pd
+
+class TfidfVectorizer(TransformerMixin):
+
+    name = "tfidf_encoder"
+
+    def __init__(self, ngram_range, max_features):
+        self.preprocessor = None
+        self.ngram_range = ngram_range
+        self.max_features = max_features
+
+    def fit(self, X, y=None):
+        self.preprocessor = TV(ngram_range=self.ngram_range, max_features=self.max_features)
+        self.preprocessor.fit(X.apply(lambda x : " ".join(x), axis=1))
+        return self
+    
+    def transform(self, X):
+        data = self.preprocessor.transform(X.apply(lambda x : " ".join(x), axis=1)).toarray()
+        return pd.DataFrame(data=data, columns=[f"text_{i}" for i in range(len(data[0]))])
+
+ngram_range = CategoricalHyperparameter("ngram_range", [(i,j) for i in range(1,5) for j in range(1,5) if i<=j])
+max_features = IntegerHyperparameter("max_features", 1, 100)
+Registry.add_component_to_registry(TEXT_ENCODING, [ngram_range, max_features], TfidfVectorizer)    
+
+
