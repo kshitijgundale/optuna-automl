5e547d655705847e6c9c8eee78217934522e8329
kshitijgundale <kshitijgundale08@gmail.com>
2022-10-25 17:28:00 +0530

    moved time-budget parameter to train(), added add_pipe()

--- optuna_automl/automl.py
+++ optuna_automl/automl.py
@@ -1,48 +1,43 @@
-import pandas as pd
-import optuna
+from optuna_automl.pipeline import AutomlPipeline
 from sklearn.model_selection import cross_val_score
-from optuna_automl.pipelines.basic_pipeline import basic_pipeline
+import optuna
+import copy
 
 class AutoML():
-    
-    def __init__(
-        self,
-        ml_task='auto',
-        time_budget=3600,
-        cv=5,
-        feat_types=None
-    ): 
-        self.ml_task = self._ml_task()
-        self.time_budget = time_budget
+
+    def __init__(self, cv=5):
         self.cv = cv
-        self.feat_types = feat_types
 
-    def _ml_task(self):
-        return self.ml_task 
-
-    def objective(trial, X, y, feat_types, cv):
-        ppl = AutomlPipeline(steps=basic_pipeline(X, y, feat_types=feat_types))
-        ppl.set_trial_params(trial, "main")
-        
-        score = cross_val_score(ppl, X, y, n_jobs=-1, cv=cv, scoring='accuracy', error_score="raise")
-        accuracy = score.mean()
-
-        return 1 - accuracy
-
-    def train(self, data, target):
-        X = data.drop([target], axis=1)
-        y = data[target]
-        feat_types = self.feat_types
-        cv = self.cv
+        self.best_params = None
+        self._ppl = AutomlPipeline(steps=[])
+    
+    def train(self, data, target, time_budget=10800):
+        if not self.get_pipeline_steps():
+            Exception("No components added to pipeline. Add components to pipeline using add_pipe or use available pipelines")
 
         study = optuna.create_study(direction="minimize")
-        study.optimize(lambda trial : AutoML.objective(trial, X, y, feat_types, cv), timeout=self.time_budget)
+        study.optimize(lambda trial: self._objective_func(trial, data, target), timeout=time_budget)
 
-        study.best_params
+        self.best_params = study.best_params
+        self._ppl.set_params(self.best_params, "main")
+        self._ppl.fit(data, target)
 
-        return self
+    def _objective_func(self, trial, X, y):
+        ppl = copy.deepcopy(self._ppl)
+        ppl.set_trial_params(trial, "main")
+        score = cross_val_score(ppl, X, y, n_jobs=-1, cv=self.cv, scoring='accuracy', error_score="raise")
+        accuracy = score.mean()
+        return 1 - accuracy    
 
-    def predict(data):
+    def predict(self, X):
         pass
 
+    def get_ml_task(self, X, y):
+        return "classification"
+
+    def get_pipeline_steps(self):
+        return self._ppl.steps
+
+    def add_pipe(self, name, task):
+        self._ppl.add_pipe(name, task)
         
