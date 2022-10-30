e9db245342a909fc8a82b58336429e567473545c
kshitijgundale <kshitijgundale08@gmail.com>
2022-10-31 01:42:22 +0530

    added code for building and passing ml_params, timing optuna study

--- optuna_automl/automl.py
+++ optuna_automl/automl.py
@@ -2,6 +2,7 @@ from optuna_automl.pipeline import AutomlPipeline
 from sklearn.model_selection import cross_val_score
 import optuna
 import copy
+import time
 
 class AutoML():
 
@@ -10,13 +11,23 @@ class AutoML():
 
         self.best_params = None
         self._ppl = AutomlPipeline(steps=[])
+        self.ml_task = None
+        self.ml_params = {}
     
     def train(self, data, target, time_budget=10800):
         if not self.get_pipeline_steps():
             Exception("No components added to pipeline. Add components to pipeline using add_pipe or use available pipelines")
 
+        ## Create ml params
+        self.ml_task = self.ml_task or self.get_ml_task(data, target)
+        self.ml_params['ml_task'] = self.ml_task
+
+        ## Create optuna study
         study = optuna.create_study(direction="minimize")
+        s = time.perf_counter()
         study.optimize(lambda trial: self._objective_func(trial, data, target), timeout=time_budget)
+        e = time.perf_counter()
+        self.time_taken = e - s
 
         self.best_params = study.best_params
         self._ppl.set_params(self.best_params, "main")
@@ -24,7 +35,7 @@ class AutoML():
 
     def _objective_func(self, trial, X, y):
         ppl = copy.deepcopy(self._ppl)
-        ppl.set_trial_params(trial, "main")
+        ppl.set_trial_params(trial, "main", self.ml_params)
         score = cross_val_score(ppl, X, y, n_jobs=-1, cv=self.cv, scoring='accuracy', error_score="raise")
         accuracy = score.mean()
         return 1 - accuracy    
