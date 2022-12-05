30621ee06c813a9d384669969d05f38b94f12639
kshitijgundale <kshitijgundale08@gmail.com>
2022-12-05 14:39:09 +0530

    pipeline components are now added inside train method

--- optuna_automl/pipelines/basic_pipeline.py
+++ optuna_automl/pipelines/basic_pipeline.py
@@ -7,20 +7,27 @@ from optuna_automl.pipeline import AutomlPipeline
 
 class BasicPipeline(AutoML):
 
-    def __init__(self, data, target, cv=5, ml_task="auto", feat_types=None):
+    def __init__(self, cv=5, ml_task="auto", feat_types=None):
+
+        self._ml_task = ml_task
+        self.feat_types = feat_types
 
         super().__init__(cv=cv)
 
+    def train(self, data, target, time_budget=10000):
         X, y = self._prepare_data(data, target)
 
-        if ml_task == "auto":
+        if self._ml_task == "auto":
             self.ml_task = self.get_ml_task(X, y)
         else:
-            if ml_task not in ['classification', 'regression']:
+            if self._ml_task not in ['classification', 'regression']:
                 raise Exception(f"ml_task {ml_task} not recognized.")
             else:
-                self.ml_task = ml_task
+                self.ml_task = self._ml_task
 
-        self._ppl.add_pipe('data_preprocessing', DataPreprocessing(feat_types).pipeline(X, y))
-        self._ppl.add_pipe('feature_preprocessing', FEATURE_PREPROCESSING)
-        self._ppl.add_pipe('estimator', self.ml_task)
+        if not self.get_pipeline_steps():
+            self._ppl.add_pipe('data_preprocessing', DataPreprocessing(self.feat_types).pipeline(X, y))
+            self._ppl.add_pipe('feature_preprocessing', FEATURE_PREPROCESSING)
+            self._ppl.add_pipe('estimator', self.ml_task)   
+
+        self._fit(data, target, time_budget)     
